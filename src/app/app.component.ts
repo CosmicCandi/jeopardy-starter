545690bad8d93ec58af665f556d265e389919795
CosmicCandi <kalea.wolff@gmail.com>
2017-09-21 19:03:07 -0700

    Initial requirements met, about to split components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,28 @@ import {JeopardyService } from './jeopardy.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
-
+  title = 'Jeopardy';
+  answer: string;
+  flash: string;
+  question: string;
   questionInfo;
+  score = 0;
+
+  submitQuestion(question) {
+    if (question === this.questionInfo.answer) {
+      this.score += this.questionInfo.value;
+      this.flash = "Correct!";
+    } else {
+      this.flash = "Sorry, that was incorrect.";
+    }
+    this.getDataFromService()
+    //Clear the flash
+    setTimeout(()=> this.clearFlash(), 2000)
+ }
+
+  clearFlash() {
+    this.flash = "";
+  }
 
   constructor(private jeopardyService: JeopardyService){}
 
@@ -18,6 +37,7 @@ export class AppComponent implements OnInit {
       .subscribe(
         questionInfo => {
           this.questionInfo = questionInfo[0];
+
         }
       )
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {JeopardyService } from './jeopardy.service';
+import { ShowAnswerComponent } from './show-answer/show-answer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowAnswerComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [
     JeopardyService
